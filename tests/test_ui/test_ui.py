4c6bbf364dd5dad02beb57b8228ae1df0865ab96
Виталий Личинин <lichinin.v@yandex.ru>
2025-05-10 16:48:54 +0300

    xfail mark

--- tests/test_ui/test_ui.py
+++ tests/test_ui/test_ui.py
@@ -1,4 +1,5 @@
 from locators.locators import Selectors
+import pytest
 
 
 def test_ui(open_start_page):
@@ -6,7 +7,7 @@ def test_ui(open_start_page):
     page.get_element(Selectors.DYNAMIC).click()
     assert "Dynamic" in page.browser.title
 
-
+@pytest.mark.xfail(reason='Намеренный провал')
 def test_ui_broken(open_start_page):
     page = open_start_page
     page.get_element(Selectors.DYNAMIC).click()
